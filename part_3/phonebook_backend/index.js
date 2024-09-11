e4223cfab4d1ad3f22a89b5a508a37afacec022d
levitesuo <leevi.suotula@gmail.com>
2024-09-11 14:23:52 +0300

    tried to do stuff in part 3

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,41 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/info', (request, response) => {
-  local content = {
-    <p>Phonebook has info for 2 people</p>
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
   }
-  response.send(content)
-})
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server at ${PORT}`)
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => {
+    person.id !== id;
+  });
+  response.status(204).end();
+});
+
+app.get("/info", (request, response) => {
+  let first_line = `<p>Phonebook has info for ${persons.length} people.</p>`;
+  let second_line = `${Date(Date.now()).toString()}`;
+  response.send(first_line + second_line);
+});
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server at ${PORT}`);
